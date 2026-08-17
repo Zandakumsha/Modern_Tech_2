c297b7d4d301bdf96f33a0bee40dcf2a7097d491
Zandakumsha <zandaj.kumsha@gmail.com>
2026-08-17 22:14:04 +0200

    feat: add employee API routes

---
+++ src/routes/employees.routes.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+  listEmployees,
+  getEmployee,
+  createEmployeeHandler,
+  updateEmployeeHandler,
+  deleteEmployeeHandler,
+  getPayroll,
+  getAttendance,
+  getLeave,
+  requestLeave,
+  getSummary,
+} from "../controllers/employeeController.js";
+
+const router = express.Router();
+
+router.get("/", listEmployees);
+router.post("/", createEmployeeHandler);
+
+router.get("/:id", getEmployee);
+router.put("/:id", updateEmployeeHandler);
+router.delete("/:id", deleteEmployeeHandler);
+
+router.get("/:id/summary", getSummary);
+router.get("/:id/payroll", getPayroll);
+router.get("/:id/attendance", getAttendance);
+router.get("/:id/leave", getLeave);
+router.post("/:id/leave", requestLeave);
+
+export default router;
