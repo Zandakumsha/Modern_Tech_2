cb70b13eae83abbb98e0e0b19a6c8989fcb380de
Zanda Kumsha <zandaj.kumsha@gmail.com>
2026-08-18 15:53:59 +0200

    Fix roles middleware conflict markers

--- src/middleware/roles.js
+++ src/middleware/roles.js
@@ -3,11 +3,7 @@ export function requireRole(...allowedRoles) {
     if (!req.user || !allowedRoles.includes(req.user.role)) {
       return res.status(403).json({ message: "You do not have permission to access this area" });
     }
-<<<<<<< HEAD
     return next();
-=======
-    next();
->>>>>>> 0338b7b7c12264ed14f0d522ed575d1042677b07
   };
 }
 
