b349e104c55ef569c70842fa35788a0cb44f4cda
Zandakumsha <zandaj.kumsha@gmail.com>
2026-08-19 20:49:48 +0200

    Add delete notification route

--- src/routes/notifications.routes.js
+++ src/routes/notifications.routes.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
 import { requireHR, requireEmployee } from "../middleware/roles.js";
-import { getNotifications, createNotification, markNotificationRead, markAllNotificationsRead } from "../controllers/notificationsController.js";
+import { getNotifications, createNotification, markNotificationRead, markAllNotificationsRead, deleteNotification } from "../controllers/notificationsController.js";
 
 const router = Router();
-
 router.get("/", auth, requireHR, getNotifications);
 router.post("/", auth, requireEmployee, createNotification);
 router.patch("/:id/read", auth, requireHR, markNotificationRead);
 router.patch("/read-all", auth, requireHR, markAllNotificationsRead);
-
+router.delete("/:id", auth, requireHR, deleteNotification);
 export default router;
