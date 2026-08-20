2eecb698e7a7d4241c8ae57f03934467b8c5ca0f
Zandakumsha <zandaj.kumsha@gmail.com>
2026-08-20 19:13:36 +0200

    add employee CRUD API routes

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,9 +1,14 @@
 import express from "express";
 import auth from "../middleware/auth.js";
 import { getMyEmployeeProfile } from "../controllers/employeeController.js";
+import { listEmployees, createEmployee, updateEmployee, deleteEmployee } from "../controllers/employeesController.js";
 
 const router = express.Router();
 
 router.get("/me", auth, getMyEmployeeProfile);
+router.get("/", auth, listEmployees);
+router.post("/", auth, createEmployee);
+router.put("/:employeeId", auth, updateEmployee);
+router.delete("/:employeeId", auth, deleteEmployee);
 
 export default router;
