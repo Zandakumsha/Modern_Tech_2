a8893fb2bc07712a2287cecc2b54ae51eb50e20b
Zandakumsha <zandaj.kumsha@gmail.com>
2026-08-20 11:22:38 +0200

    Use authenticated HR username for calendar events

--- src/routes/calendar.routes.js
+++ src/routes/calendar.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import auth from "../middleware/auth.js";
 import { createEvent, deleteEvent, listEvents } from "../controllers/calendarController.js";
 
 const router = Router();
+
+router.use(auth);
 
 router.get("/", listEvents);
 router.post("/", createEvent);
